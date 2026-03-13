74f649dc90c5617a45cfb15627c050f881fefd31
Harshita Singh <harshita.singh@verifone.com>
2026-03-13 15:25:43 +0100

    added types, error handling in service

--- src/app/core/service/country.service.ts
+++ src/app/core/service/country.service.ts
@@ -1,31 +1,64 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable, of } from "rxjs";
+import { country } from "../../shared/types/country.type";
+import { catchError } from "rxjs/operators";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class CountryService {
+  private apiBaseUrl = "https://restcountries.com/v3.1";
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
-
-  getAll(): Observable<any> {
-    return this.http.get('https://restcountries.com/v3.1/all?fields=name,capital,region,population,area,currencies,languages,borders,flags,cca3');
+  getAll(): Observable<country[]> {
+    return this.http
+      .get<
+        country[]
+      >(`${this.apiBaseUrl}/all?fields=name,capital,region,population,area,currencies,languages,flags`)
+      .pipe(
+        catchError((error) => {
+          console.log("Error while fetching list of all contries:", error);
+          return of([]);
+        }),
+      );
   }
 
-  getByCode(code: string): Observable<any> {
-    return this.http.get('https://restcountries.com/v3.1/alpha/' + code);
+  getByCode(code: string): Observable<country[]> {
+    return this.http.get<country[]>(`${this.apiBaseUrl}/alpha/${code}`).pipe(
+      catchError((error) => {
+        console.log("Error while fetching countries by code:", error);
+        return of([]);
+      }),
+    );
   }
 
-  searchByName(name: string): Observable<any> {
-    return this.http.get('https://restcountries.com/v3.1/name/' + name);
+  searchByName(name: string): Observable<country[]> {
+    return this.http.get<country[]>(`${this.apiBaseUrl}/name/${name}`).pipe(
+      catchError((error) => {
+        console.log("Error while fetching countries by name:", error);
+        return of([]);
+      }),
+    );
   }
 
-  getByRegion(region: string): Observable<any> {
-    return this.http.get('https://restcountries.com/v3.1/region/' + region);
+  getByRegion(region: string): Observable<country[]> {
+    return this.http.get<country[]>(`${this.apiBaseUrl}/region/${region}`).pipe(
+      catchError((error) => {
+        console.log("Error while fetching countries by region:", error);
+        return of([]);
+      }),
+    );
   }
 
-  getByCodes(codes: string[]): Observable<any> {
-    return this.http.get('https://restcountries.com/v3.1/alpha?codes=' + codes.join(','));
+  getByCodes(codes: string[]): Observable<country[]> {
+    return this.http
+      .get<country[]>(`${this.apiBaseUrl}/alpha?codes=${codes.join(",")}`)
+      .pipe(
+        catchError((error) => {
+          console.log("Error while fetching countries by code:", error);
+          return of([]);
+        }),
+      );
   }
 }
--- src/app/countries/country-card/country-card.component.ts
+++ src/app/countries/country-card/country-card.component.ts
@@ -1,39 +1,39 @@
 import { Component, Input } from '@angular/core';
 import { Router } from '@angular/router';
+import { country, Currency } from "../../shared/types/country.type";
 
 @Component({
-  selector: 'app-country-card',
+  selector: "app-country-card",
   standalone: false,
-  templateUrl: './country-card.component.html'
+  templateUrl: "./country-card.component.html",
 })
 export class CountryCardComponent {
+  @Input() country!: country;
 
-  @Input() country: any;
-
-  constructor(private router: Router) { }
+  constructor(private router: Router) {}
 
   getPopulation(): string {
     if (this.country.population > 1000000) {
-      return (this.country.population / 1000000).toFixed(1) + 'M';
+      return (this.country.population / 1000000).toFixed(1) + "M";
     } else if (this.country.population > 1000) {
-      return (this.country.population / 1000).toFixed(1) + 'K';
+      return (this.country.population / 1000).toFixed(1) + "K";
     }
     return this.country.population.toString();
   }
 
   getCurrencies(): string {
-    if (!this.country.currencies) return 'N/A';
+    if (!this.country.currencies) return "N/A";
     return Object.values(this.country.currencies)
-      .map((c: any) => c.name)
-      .join(', ');
+      .map((c: Currency) => c.name)
+      .join(", ");
   }
 
   getLanguages(): string {
-    if (!this.country.languages) return 'N/A';
-    return Object.values(this.country.languages).join(', ');
+    if (!this.country.languages) return "N/A";
+    return Object.values(this.country.languages).join(", ");
   }
 
   onCardClick(): void {
-    console.log('Country clicked:', this.country.name.common);
+    console.log("Country clicked:", this.country.name.common);
   }
 }
